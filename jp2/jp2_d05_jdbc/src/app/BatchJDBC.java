4990ba233c2a87f16448a17fa30e90788d55963f
Le Mong Thuy <thuyle2000@gmail.com>
2023-01-10 18:50:08 +0700

    Upload Jan 10, 2023

--- jp2/jp2_d05_jdbc/src/app/BatchJDBC.java
+++ jp2/jp2_d05_jdbc/src/app/BatchJDBC.java
@@ -9,16 +9,16 @@ import java.util.*;
 public class BatchJDBC {
 
     public static void main(String[] args) {
-        BatchJDBC p= new BatchJDBC();
+        BatchJDBC p = new BatchJDBC();
         p.menu();
     }
-    
-    private void menu(){
-        String op ="";
+
+    private void menu() {
+        String op = "";
         Scanner sc = new Scanner(System.in);
-        
+
         //xay dung menu
-        while(true){
+        while (true) {
             System.out.println("\n  === MENU === ");
             System.out.println("1. xem ds cac lop hoc");
             System.out.println("2. them lop hoc moi");
@@ -28,12 +28,17 @@ public class BatchJDBC {
             System.out.println("=========================");
             System.out.print(" nhap ma so chuc nang [1-5]: ");
             op = sc.nextLine().trim();
-            
+
             switch (op) {
                 case "1" -> {
                     display();
                 }
-                case "2" -> insert();
+                case "2" ->
+                    insert();
+                case "3" ->
+                    updateFee();
+                case "4" ->
+                    delete();
                 case "5" -> {
                     return;
                 }
@@ -46,11 +51,11 @@ public class BatchJDBC {
 
     private void display() {
         ArrayList<Batch> ds = BatchDAO.get();
-        if(ds==null){
+        if (ds == null) {
             System.out.println("\n >> He thong chua co du lieu ");
             return;
         }
-        
+
         System.out.println("\n Danh sach cac lop hoc");
         ds.stream().forEach(System.out::println);
     }
@@ -59,13 +64,45 @@ public class BatchJDBC {
         Batch b = new Batch();
         System.out.println("\n >> nhap thong tin lop hoc moi: ");
         b.input();
-        
-        if(BatchDAO.insert(b)>0){
+
+        if (BatchDAO.insert(b) > 0) {
             System.out.println(" >> Them lop hoc moi thanh cong !");
-        }
-        else{
+        } else {
             System.out.println(" >> Them lop hoc moi that bai !");
         }
     }
-    
+
+    private void updateFee() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n >> nhap ma lop hoc : ");
+        String id = sc.nextLine().trim();
+
+        Batch b = BatchDAO.get(id);
+        //kiem tra lop hoc voi ma so [id] co ton tai trong DB
+        if (b == null) {
+            System.out.printf(" >> Loi: lop hoc co ma so [%s] ko ton tai trong he thong !\n", id);
+        } else {
+            System.out.print(" >> nhap hoc phi moi: ");
+            b.fee = Integer.parseInt(sc.nextLine().trim());
+            if (BatchDAO.update(id, b) > 0) {
+                System.out.println(" >> Thay doi hoc phi moi thanh cong !");
+            } else {
+                System.out.println(" >> Loi: thay doi hoc phi that bai !");
+            }
+        }
+    }
+
+    private void delete() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n >> nhap ma lop hoc : ");
+        String id = sc.nextLine().trim();
+        
+        if(BatchDAO.delete(id)>0){
+            System.out.println(" >> Da xoa lop hoc thanh cong !");
+        }
+        else{
+            System.out.println(" >> Loi: Xoa lop hoc that bai !");
+        }
+    }
+
 }
--- jp2/jp2_d05_jdbc/src/model/BatchDAO.java
+++ jp2/jp2_d05_jdbc/src/model/BatchDAO.java
@@ -7,86 +7,196 @@ import java.sql.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class BatchDAO {
-    
+
     //1. ham tra ve ds cac lop hoc
-    public static ArrayList<Batch> get(){     
-        
+    public static ArrayList<Batch> get() {
+
         //1. tao ket noi den CSDL
         Connection cn = MyLIB.getConnect();
-        if(cn == null){
+        if (cn == null) {
             System.out.println(">> Loi sai ket noi CSDL");
             return null;
         }
-        
+
         ArrayList<Batch> ds = new ArrayList<>();
         try {
             //2. tao doi tuong Statement chua lenh truy van SQl
             Statement st = cn.createStatement();
-            
+
             //3. thuc hien lenh SELECT 
             String sql = "select * from tbBatch";
-            ResultSet rs =  st.executeQuery(sql);
-            
+            ResultSet rs = st.executeQuery(sql);
+
             //4. duyet cac dong trong rs
-            while(rs.next()){
+            while (rs.next()) {
                 //doc tung cot tren dong, tao ra 1 doi tuong lop hoc tuong ung
                 Batch b = new Batch(rs.getString("batchno"), rs.getString("description"), rs.getInt("fee"));
-                
+
 //                Batch ba = new Batch();
 //                ba.batchno = rs.getString("batchno");
 //                ba.description = rs.getString(2);
 //                ba.fee = rs.getInt(3);
-                
                 ds.add(b);
             }
-            
+
             //5. dong cac tai nguyen
             rs.close();
             st.close();
             cn.close();
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
         return ds;
     }
-    
-    
-    //2. ham them 1 lop hoc moi vo Database
-    public static int insert(Batch b){
-        int r = 0;
-        
+
+    //1. ham tra ve lop hoc co ma so id
+    public static Batch get(String id) {
+
         //1. tao ket noi den CSDL
         Connection cn = MyLIB.getConnect();
-        if(cn == null){
+        if (cn == null) {
+            System.out.println(">> Loi sai ket noi CSDL");
+            return null;
+        }
+
+        Batch b = null;
+        try {
+            //2. tao doi tuong Statement chua lenh truy van SQl
+            Statement st = cn.createStatement();
+
+            //3. thuc hien lenh SELECT 
+            String sql = "select * from tbBatch where batchno like '" + id + "'";
+            ResultSet rs = st.executeQuery(sql);
+
+            //4. duyet cac dong trong rs
+            if (rs.next()) {
+                //doc tung cot tren dong, tao ra 1 doi tuong lop hoc tuong ung
+                b = new Batch();
+                b.batchno = rs.getString("batchno");
+                b.description = rs.getString(2);
+                b.fee = rs.getInt(3);
+            }
+
+            //5. dong cac tai nguyen
+            rs.close();
+            st.close();
+            cn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return b;
+    }
+
+    //2. ham them 1 lop hoc moi vo Database
+    public static int insert(Batch b) {
+        int r = 0;
+
+        //1. tao ket noi den CSDL
+        Connection cn = MyLIB.getConnect();
+        if (cn == null) {
             System.out.println(">> Loi sai ket noi CSDL");
             return r;
-        } 
- 
+        }
+
         try {
             //2. tao doi tuong prepareStatement de chua lenh insert sql
             String sql = "INSERT INTO tbBatch VALUES (?,?,?)";
             PreparedStatement st = cn.prepareStatement(sql);
-            
+
             //3. set gia tri cho cac tham so ? bang cac thuoc tinh tuong ung cua lop hoc [b]
             st.setString(1, b.batchno);
             st.setString(2, b.description);
             st.setInt(3, b.fee);
-            
+
             //4. thuc hien lenh INSERT
             r = st.executeUpdate();
-            
+
             //5. dong cac tai nguyen
             st.close();
             cn.close();
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
+        return r;
+    }
+
+    //3. ham cap nhat viec thay doi thong tin cua 1 lop hoc da co
+    public static int update(String id, Batch b) {
+        int r = 0;
+
+        //1. tao ket noi den CSDL
+        Connection cn = MyLIB.getConnect();
+        if (cn == null) {
+            System.out.println(">> Loi sai ket noi CSDL");
+            return r;
+        }
+
+        try {
+            //2. tao doi tuong prepareStatement de chua lenh update sql
+            String sql = "UPDATE tbBatch set [description] = ?, [fee] = ? where [batchno] like ? ";
+            PreparedStatement st = cn.prepareStatement(sql);
+
+            //3. set gia tri cho cac tham so ? bang cac thuoc tinh tuong ung cua lop hoc [b]
+            st.setString(1, b.description);
+            st.setInt(2, b.fee);
+            st.setString(3, id);
+
+            //4. thuc hien lenh UPDATE
+            r = st.executeUpdate();
+
+            //5. dong cac tai nguyen
+            st.close();
+            cn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return r;
+    }
+
+    //4. ham xoa 1 lop hoc theo ma so
+    public static int delete(String id) {
+        int r = 0;
+
+        //1. tao ket noi den CSDL
+        Connection cn = MyLIB.getConnect();
+        if (cn == null) {
+            System.out.println(">> Loi sai ket noi CSDL");
+            return r;
+        }
+
+        PreparedStatement st = null;
+        try {
+            //2. tao doi tuong prepareStatement de chua lenh DELETE sql
+            String sql = "DELETE FROM [tbBatch] where [batchno] like ? ";
+            st = cn.prepareStatement(sql);
+
+            //3. set gia tri cho cac tham so ? bang cac thuoc tinh tuong ung cua lop hoc [b]
+            st.setString(1, id);
+
+            //4. thuc hien lenh DELETE
+            r = st.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            //5. dong cac tai nguyen       
+            try {
+                st.close();
+                cn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(BatchDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         return r;
     }
 }
